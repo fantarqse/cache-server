367ff73ada16c45d359a396e48731b67b3ebd6d8
fantarqse <fantarose51@gmail.com>
2024-05-02 17:13:47 +0300

    chore: removed log

--- cmd/cache.go
+++ cmd/cache.go
@@ -1,8 +1,6 @@
 package cachecmd
 
 import (
-	"log"
-
 	"cache-server/internal/cache"
 	"cache-server/internal/config"
 	"cache-server/internal/server"
@@ -14,13 +12,11 @@ func Run() error {
 		return err
 	}
 
-	log.Println("a redis is running")
 	redis := cache.New(cfg.Redis)
 
 	srv := server.New(redis)
 	srv.Routes()
 
-	log.Println("a server is running")
 	if err := srv.Run(cfg.Server.Port); err != nil {
 		return err
 	}
